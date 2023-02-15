586a903cea683bcaa2f6a1079c514f2c9b4d17d0
malak-wahyb <malakwahyb537@gmail.com>
2023-02-15 20:32:32 +0200

    Step 7 :PHP Control Statements -  If / else

---
+++ basic1/step_7.php
@@ -0,0 +1,10 @@
+<?php
+$birth_year=1999;
+$current_year=2023;
+$different=$current_year-$birth_year;
+if($different>=18){
+    echo "you can drive";
+}else{
+    echo "You still a kid, go and play GTA";
+}
+?>
