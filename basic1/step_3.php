55d179dfafe917609858fd99df903e4f581394b8
malak-wahyb <malakwahyb537@gmail.com>
2023-02-15 18:55:53 +0200

    Step 3 : Variables lvl 1

---
+++ basic1/step_3.php
@@ -0,0 +1,10 @@
+<?php
+$name="Malak";
+$email="malakwahyb537@gmail.com";
+echo "my name is " .$name." my email is ".$email."<br>" ;
+/////
+$height=100;
+$width=85;
+$area=$width*$height;
+echo $area
+?>
