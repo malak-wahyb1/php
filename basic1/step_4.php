24e8cdd5783495e1e5cd2e8786083b1e85a71d36
malak-wahyb <malakwahyb537@gmail.com>
2023-02-15 19:39:55 +0200

    Step 4 : Strings Concatenation 1

---
+++ basic1/step_4.php
@@ -0,0 +1,11 @@
+<?php
+$name="Malak wahyb";
+$position="developer";
+$github_url="https://github.com/malak-wahyb1";
+
+echo "I am " .$name. " and I am " .$position. " please check my github link " .$github_url."<br>";
+echo "I am $name and I am $position please check my github link $github_url <br>";
+echo 'I am ' .$name. ' and I am ' .$position. ' please check my github link ' .$github_url. '<br>';
+echo "I am {$name} and I am {$position} please check my github link {$github_url}"
+
+?>
